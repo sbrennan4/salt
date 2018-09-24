80358059fa47c197cf4b8e7df2b445d34789539d
Austin Papp <austin.papp@gmail.com>
2018-09-24 11:14:59 -0400

    Adding new script engine

--- salt/engines/script.py
+++ salt/engines/script.py
@@ -53,23 +53,25 @@ def _get_serializer(output):
     '''
     serializers = salt.loader.serializers(__opts__)
     try:
-        return getattr(serializers, output)
+        serializer = getattr(serializers, output)
     except AttributeError:
         raise CommandExecutionError("Unknown serializer '%s' found for output option", output)
+
+    return serializer
 
 
 def start(cmd, output='json', interval=1):
     '''
     Parse stdout of a command and generate an event
-
-    The script engine will scrap stdout of the
+       
+    The script engine will scrap stdout of the 
     given script and generate an event based on the
     presence of the 'tag' key and it's value.
-
+    
     If there is a data obj available, that will also
-    be fired along with the tag.
-
-    Example:
+    be fired along with the tag. 
+    
+    Example: 
 
         Given the following json output from a script:
 
@@ -79,11 +81,12 @@ def start(cmd, output='json', interval=1):
 
         This will fire the event 'lots/of/tacos'
         on the event bus with the data obj as is.
-
+        
     :param cmd: The command to execute
     :param output: How to deserialize stdout of the script
     :param interval: How often to execute the script.
     '''
+
     try:
         cmd = shlex.split(cmd)
     except AttributeError:
@@ -105,13 +108,13 @@ def start(cmd, output='json', interval=1):
             proc = subprocess.Popen(cmd,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.STDOUT)
-
-            log.debug("Starting script with pid %d", proc.pid)
+        
+            log.debug("Starting script with pid %d", proc.pid) 
 
             for raw_event in _read_stdout(proc):
                 log.debug(raw_event)
 
-                event = serializer.deserialize(raw_event)
+                event = serializer.deserialize(raw_event) 
                 tag = event.get('tag', None)
                 data = event.get('data', {})
 
@@ -126,7 +129,7 @@ def start(cmd, output='json', interval=1):
             proc.stdout.close()
             rc = proc.wait()
             if rc:
-                raise subprocess.CalledProcessError(rc, cmd)
+               raise subprocess.CalledProcessError(rc, cmd)
 
         except subprocess.CalledProcessError as e:
             log.error(e)
