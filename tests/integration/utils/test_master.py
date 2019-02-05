6cd4e6a5795a5f5582f06b3795fdb0503670a191
Austin Papp <apapp@bloomberg.net>
2019-02-05 15:02:34 -0500

    Track jid in proc file for runners
    * Added support functions to handle reading proc files master side
    * Added clean proc dir for master side
    * Added dateutil and setproctile to tests.txt
    * async option for ShellTestCase/ShellCase

--- tests/integration/utils/test_master.py
+++ tests/integration/utils/test_master.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-'''
-Test master code from utils
-'''
 from __future__ import absolute_import
 
 import os
@@ -14,7 +11,6 @@ import salt.utils.master as master
 
 from tests.support.case import ShellTestCase
 from tests.support.paths import TMP_ROOT_DIR
-from tests.support.helpers import flaky
 
 DEFAULT_CONFIG = salt.config.master_config(None)
 DEFAULT_CONFIG['cachedir'] = os.path.join(TMP_ROOT_DIR, 'cache')
@@ -25,16 +21,12 @@ class MasterUtilJobsTestCase(ShellTestCase):
     def setUp(self):
         # Necessary so that the master pid health check
         # passes as it looks for salt in cmdline
-        setproctitle.setproctitle('salt')
+        setproctitle.setproctitle('salt')  # pylint: disable=c-extension-no-member
 
-    @flaky
     def test_get_running_jobs(self):
-        '''
-        Test get running jobs
-        '''
         ret = self.run_run_plus("test.sleep", '90', asynchronous=True)
         jid = ret['jid']
-        time.sleep(20)
+        time.sleep(2)
         jobs = master.get_running_jobs(DEFAULT_CONFIG)
         jids = [job['jid'] for job in jobs]
         assert jids.count(jid) == 1
