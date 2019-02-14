1f5f64db0bff16662695803f373aff0525671e8d
Austin Papp <apapp@bloomberg.net>
2019-02-14 10:20:06 -0500

    increase wait queue time

--- tests/integration/reactor/test_reactor.py
+++ tests/integration/reactor/test_reactor.py
@@ -35,4 +35,89 @@ class ReactorTest(ModuleCase, SaltMinionEventAssertsMixin):
 
         e.fire_event({'a': 'b'}, '/test_event')
 
-        self.assertMinionEventReceived({'a': 'b'})
+        self.assertMinionEventReceived({'a': 'b'}, queue_wait=5)
+
+    @skipIf(salt.utils.platform.is_windows(), 'no sigalarm on windows')
+    def test_reactor_reaction(self):
+        '''
+        Fire an event on the master and ensure
+        The reactor event responds
+        '''
+        signal.signal(signal.SIGALRM, self.alarm_handler)
+        signal.alarm(self.timeout)
+
+        master_event = self.get_event()
+        master_event.fire_event({'id': 'minion'}, 'salt/test/reactor')
+
+        try:
+            while True:
+                event = master_event.get_event(full=True)
+
+                if event is None:
+                    continue
+
+                if event.get('tag') == 'test_reaction':
+                    self.assertTrue(event['data']['test_reaction'])
+                    break
+        finally:
+            signal.alarm(0)
+
+    @skipIf(salt.utils.platform.is_windows(), 'no sigalarm on windows')
+    def test_reactor_is_leader(self):
+        '''
+        when leader is set to false reactor should timeout/not do anything
+        '''
+        # by default reactor should be leader
+        ret = self.run_run_plus('reactor.is_leader')
+        self.assertTrue(ret['return'])
+
+        # make reactor not leader
+        self.run_run_plus('reactor.set_leader', False)
+        ret = self.run_run_plus('reactor.is_leader')
+        self.assertFalse(ret['return'])
+
+        signal.signal(signal.SIGALRM, self.alarm_handler)
+        signal.alarm(self.timeout)
+
+        try:
+            master_event = self.get_event()
+            self.fire_event({'id': 'minion'}, 'salt/test/reactor')
+
+            while True:
+                event = master_event.get_event(full=True)
+
+                if event is None:
+                    continue
+
+                if event.get('tag') == 'test_reaction':
+                    # if we reach this point, the test is a failure
+                    self.assertTrue(False)  # pylint: disable=redundant-unittest-assert
+                    break
+        except TimeoutException as exc:
+            self.assertTrue('Timeout' in str(exc))
+        finally:
+            signal.alarm(0)
+
+        # make reactor leader again
+        self.run_run_plus('reactor.set_leader', True)
+        ret = self.run_run_plus('reactor.is_leader')
+        self.assertTrue(ret['return'])
+
+        # trigger a reaction
+        signal.alarm(self.timeout)
+
+        try:
+            master_event = self.get_event()
+            self.fire_event({'id': 'minion'}, 'salt/test/reactor')
+
+            while True:
+                event = master_event.get_event(full=True)
+
+                if event is None:
+                    continue
+
+                if event.get('tag') == 'test_reaction':
+                    self.assertTrue(event['data']['test_reaction'])
+                    break
+        finally:
+            signal.alarm(0)
