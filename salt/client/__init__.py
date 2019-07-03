eebc0032a36ebac12fbf3f695f06b11dae687dd3
Matt Phillips <mphillips81@bloomberg.net>
2019-07-02 20:11:37 -0400

    improve long polling performance / subscription tracking
    when dealing with long running jobs via cmd_iter, the heartbeat find_job
    calls would continually add new event.subscribe() tag match
    subscriptions for the jobs as they come in. This works out to a slow
    leak over time when dealing with a job that runs over 12 hours+, and
    adds delays when the real job ret finally does come in. This improves
    the subscription handling to unsub the previous saltutil.find_job calls
    events if set before broadcasting the next.

--- salt/client/__init__.py
+++ salt/client/__init__.py
@@ -245,6 +245,7 @@ class LocalClient(object):
                                 **kwargs
                                )
 
+        # we should already be subscribed to the response, but incase we are not
         if 'jid' in pub_data:
             self.event.subscribe(pub_data['jid'])
 
@@ -1192,6 +1193,7 @@ class LocalClient(object):
         else:
             ret_iter = self.get_returns_no_block('salt/job/{0}'.format(jid))
         # iterator for the info of this job
+        jinfo = None
         jinfo_iter = []
         # open event jids that need to be un-subscribed from later
         open_jids = set()
@@ -1260,6 +1262,12 @@ class LocalClient(object):
             # if the jinfo has timed out and some minions are still running the job
             # re-do the ping
             if time.time() > timeout_at and minions_running:
+                # unsubscribe from previous find_job ping before sending out
+                # the new broadcast if needed, otherwise long running jobs will
+                # accumulate and be computationally expensive to finish
+                if isinstance(jinfo, dict) and 'jid' in jinfo:
+                    self._clean_up_subscriptions(jinfo['jid'])
+
                 # since this is a new ping, no one has responded yet
                 jinfo = self.gather_job_info(jid, list(minions - found), 'list', **kwargs)
                 minions_running = False
@@ -1296,8 +1304,6 @@ class LocalClient(object):
                             'from client return. This may be an error in '
                             'the client.', missing_key
                         )
-                # Keep track of the jid events to unsubscribe from later
-                open_jids.add(jinfo['jid'])
 
                 # TODO: move to a library??
                 if 'minions' in raw.get('data', {}):
@@ -1346,10 +1352,10 @@ class LocalClient(object):
             else:
                 yield
 
-        # If there are any remaining open events, clean them up.
-        if open_jids:
-            for jid in open_jids:
-                self.event.unsubscribe(jid)
+        # unsubscribe to the last open tracking jids
+        if isinstance(jinfo, dict) and 'jid' in jinfo:
+            self._clean_up_subscriptions(jinfo['jid'])
+        self._clean_up_subscriptions(jid)
 
         if expect_minions:
             for minion in list((minions - found)):
@@ -1788,6 +1794,7 @@ class LocalClient(object):
 
         # if we did generate a jid or have one provided we should preemptively subscribe
         if subscribe:
+            self.event.subscribe(jid)
             if self.opts.get('order_masters'):
                 self.event.subscribe('syndic/.*/{0}'.format(jid), 'regex')
             self.event.subscribe('salt/job/{0}'.format(jid))
@@ -2038,6 +2045,7 @@ class LocalClient(object):
             del self.event
 
     def _clean_up_subscriptions(self, job_id):
+        self.event.unsubscribe(job_id)
         if self.opts.get('order_masters'):
             self.event.unsubscribe('syndic/.*/{0}'.format(job_id), 'regex')
         self.event.unsubscribe('salt/job/{0}'.format(job_id))
